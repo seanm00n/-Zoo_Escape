90fdf8652fb07911ff22fda8158858e21ccc8c6f
seanm00n <deccj97@naver.com>
2022-11-23 21:51:44 +0900

    feat player movement
    feat
    - player move
    
    add
    - CameraCtrl.cs
    
    todo
    - player rotation
    - player jump

---
+++ Assets/2. Script/CameraCtrl.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void FixedUpdate()
+    {
+        //transform.position = target.position;
+    }
+}
--- Assets/2. Script/PlayerCtrl.cs
+++ Assets/2. Script/PlayerCtrl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Transactions;
 using TreeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour {
@@ -9,49 +10,24 @@ public class PlayerCtrl : MonoBehaviour {
     public LayerMask floorLayerMask;
     public LayerMask portalLayerMask;
 
-    enum State { GROUND, AIR };
-    State state = State.GROUND;
     Rigidbody rigid;
-    float rayDistance = 1f;
-    float x;
     void Start () {
         rigid = GetComponent<Rigidbody>();
     }
-    void Update () {
-        CheckState();
-        Move();
-        CheckState();
-        Jump();
+    void FixedUpdate () {
+        Movement();
     }
-    void CheckState () {
-        if (Physics.Raycast(transform.position, -transform.up, rayDistance, floorLayerMask)
-             || Physics.Raycast(transform.position, -transform.up, rayDistance, portalLayerMask)) {
-            state = State.GROUND; // 바닥에 닿음
-        } else {
-            state =  State.AIR; // 공중에 뜸
+    void Movement () {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 vec = new Vector3(h, 0, 0);
+        if (h != 0) {
+            transform.Translate(Vector3.right*h*speed*Time.deltaTime, Space.Self); //완성!
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(vec), 100f);
         }
-    }
-    void Jump () {
-        if (state == State.GROUND) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                rigid.AddForce(new Vector3(0, 300, 0));
-                //force to vector front (x, y)
-            }
+        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //force to vector front (x, y) to jump
         }
-    }
-    void Move () {
-        if (state == State.GROUND) {
-            x = Input.GetAxis("Horizontal");
-            Vector3 vec = new Vector3(0, 0, 0);
-            vec += new Vector3(x * speed*transform.forward.x, 0f, 0f);
-            rigid.velocity = vec;
-            //가속을 없애고 싶을 때?
-            //이동 방식을 좌 우 키 입력으로 수정하기
-            //velocity/addforce/translate
-        }
-    }
-    void Rotate () {
-        //미구현
     }
     private void OnTriggerStay (Collider other) {
         Debug.Log("Portal");
@@ -59,6 +35,6 @@ public class PlayerCtrl : MonoBehaviour {
             Debug.Log("Pressed G Button");
             other.GetComponent<PortalCtrl>().MoveToTarget(transform);
         }
-        //동작하기까지 시간이 걸리는데?
+        //need frame check
     }
 }
