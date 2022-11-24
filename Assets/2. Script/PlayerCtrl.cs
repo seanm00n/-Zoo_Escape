cf0fd3c858904bab65b4623a5d31ba3cc7395423
Sean Moon <deccj97@naver.com>
2022-11-24 13:00:10 +0900

    feat rotate

--- Assets/2. Script/PlayerCtrl.cs
+++ Assets/2. Script/PlayerCtrl.cs
@@ -19,14 +19,26 @@ public class PlayerCtrl : MonoBehaviour {
         Movement();
     }
     void Movement () {
-        float h = Input.GetAxis("Horizontal");
-        if (h != 0) {
-            transform.Translate(Vector3.right*h*speed*Time.deltaTime, Space.Self); //완성!
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Vector3.up * h, 100f);
-            //Vector3.up 활용 해보자!
-            transform.Rotate(Vector3.up*h*rotSpeed);
+        Vector3 lookDirection;
+        if (Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.UpArrow) ||
+            Input.GetKey(KeyCode.DownArrow))
+        {
+            float xx = Input.GetAxisRaw("Vertical");
+            float zz = Input.GetAxisRaw("Horizontal");
+            lookDirection = xx * Vector3.forward + zz * Vector3.right;
 
+            this.transform.rotation = Quaternion.LookRotation(lookDirection);
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
+        //Vector3 dir = h * Vector3.right;
+        //if (h != 0) {
+        //    transform.Translate((h > 0 ? Vector3.forward : -Vector3.forward) * h * speed * Time.deltaTime, Space.Self); //완성!
+        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10f);
+        //    transform.rotation = Quaternion.LookRotation(dir, -dir);
+        //    transform.Rotate(h > 0 ? Vector3.up : Vector3.down);
+        //}
         
         if (Input.GetKeyDown(KeyCode.Space)) {
             //force to vector front (x, y) to jump
