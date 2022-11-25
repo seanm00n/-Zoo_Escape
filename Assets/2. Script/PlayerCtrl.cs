419a3be6186612c912c9a3e7b8eca5dcb4f89322
Sean Moon <deccj97@naver.com>
2022-11-25 18:52:19 +0900

    add desc

--- Assets/2. Script/PlayerCtrl.cs
+++ Assets/2. Script/PlayerCtrl.cs
@@ -19,7 +19,7 @@ public class PlayerCtrl : MonoBehaviour {
     const int maxHp = 100;
 
     float speed = 10f;
-    float rayDistance = 1f;
+    float rayDistance = 1.05f;
     float h;
 
     int stemina = 0;
@@ -39,25 +39,23 @@ public class PlayerCtrl : MonoBehaviour {
         h = Input.GetAxis("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space)) { //space 입력 했을 때
             if (!isActing) { //바닥에 있으면
-                rigid.AddForce(new Vector3(0, 300, 0)); //점프
+                rigid.AddForce(new Vector3(300, 500, 0)); //점프
                 isActing = true;
-                return;
             }
         }
+        Debug.DrawRay(transform.position,-transform.up,Color.red,rayDistance);
         if(isActing && Physics.Raycast(transform.position, -transform.up, rayDistance, floorLayerMask)) {
-            isActing = false;
+            isActing = false; //너무 빨리 작동해서 이동이 동작 가능해져 버림, oncoll함수로 변경
         }
-        //어디선가 isActing이 true로 멈춘다
         
-        if (Input.GetKeyDown(KeyCode.W)) 
-        {
+        if (Input.GetKeyDown(KeyCode.W))   {
             return;
         }
 
-        if (h != 0 && isActing != true ) {
+        if (h != 0 && isActing != true ) { //점프중 이동되는 버그
             transform.Translate(Vector3.right * h * speed * Time.deltaTime, Space.Self); //move
             
-            //rotate
+            //add rotate
         }
     }
     private void OnTriggerStay (Collider other) {
