1f6e6cd909aa2a6d8d8f593454bb160b09415a72
seanm00n <deccj97@naver.com>
2022-11-28 19:27:03 +0900

    fix camera bug
    fix
    - camera bug : coudnt find object

--- Assets/2. Script/CameraCtrl.cs
+++ Assets/2. Script/CameraCtrl.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
-    public Transform target;
+    Transform target;
     public float dist = 3.0f;
     public float height = 3.0f;
     public float dampTraec = 20.0f;
     public float targetHeight = 2.0f;
     private void Start() {
+        target = GameObject.Find("Player").transform;
     }
 
-/*    private void LateUpdate() {
+    private void LateUpdate () {
         //LateUpdate는 Update보다 적은 빈도로 호출됨
         transform.position = Vector3.Lerp(transform.position,
                                    target.position - (target.forward * dist) + (Vector3.up * height),
                                    Time.deltaTime * dampTraec);
         transform.LookAt(new Vector3(0, targetHeight, 0) + target.position);
-    }*/
+    }
 }
